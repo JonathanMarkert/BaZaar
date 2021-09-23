d24bcb7dd64c349d667817369cb300601f96dc8a
Johanna Skinnari <jskinnari@gmail.com>
2021-09-23 11:04:37 +0200

    #1 Set up base structure
    Co-authored-by: Jonathan Markert <JonathanMarkert@users.noreply.github.com>

---
+++ screens/AddProductSreen.tsx
@@ -0,0 +1,9 @@
+import { View, Text } from "react-native";
+
+export default function AddProductScreen() {
+  return (
+    <View>
+      <Text> Add Product </Text>
+    </View>
+  );
+}
---
+++ screens/MapScreen.tsx
@@ -0,0 +1,9 @@
+import { View, Text } from "react-native";
+
+export default function MapScreen() {
+  return (
+    <View>
+      <Text> Look a map! </Text>
+    </View>
+  );
+}
