c82c6ae042f44ad3cde0dc861a595659fdd6e6b6
Johanna Skinnari <jskinnari@gmail.com>
2021-09-24 16:25:03 +0200

    #14 change containerContent style to center

--- screens/MyScreen.tsx
+++ screens/MyScreen.tsx
@@ -31,7 +31,7 @@ const styles = StyleSheet.create({
   },
   containerContent: {
     flex: 1,
-    alignItems: "flex-start",
-    justifyContent: "flex-start",
+    alignItems: "center",
+    justifyContent: "center",
   }
 });
