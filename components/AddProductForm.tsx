06ad12465ef1cdac3be07110e00321102bd2b413
Alpha-85 <ljungdahlt@gmail.com>
2021-10-04 15:59:08 +0200

    Added 3rd party Picker to render array of categories. Basic styling on forminputs.

--- components/AddProductForm.tsx
+++ components/AddProductForm.tsx
@@ -1,20 +1,21 @@
-import { Form, Formik } from "formik";
-import React from "react";
-import * as yup from "yup";
 import { Picker } from "@react-native-picker/picker";
-
+import { Formik } from "formik";
+import React from "react";
 import {
-  TextInput,
-  View,
-  StyleSheet,
   Button,
+  KeyboardAvoidingView,
   Platform,
-  TouchableOpacity,
+  ScrollView,
+  StyleSheet,
   Text,
+  TextInput,
+  TouchableOpacity,
+  View,
 } from "react-native";
+import * as yup from "yup";
+import { categories } from "../assets/DummyData/Category";
 import { IProduct } from "../contexts/ProductContext";
 import Theme from "./Theme";
-import { categories } from "../assets/DummyData/Category";
 
 const defaultFormData: IProduct = {
   id: "",
@@ -41,7 +42,7 @@ const addProductValidation = yup.object().shape<validationSchema>({
   price: yup.number().required(),
   description: yup.string().notRequired().max(250, "keep it simple..."),
   imageUri: yup.string().notRequired(),
-  category: yup.string().notRequired(),
+  category: yup.string().required("pick one"),
   city: yup.string().required(),
   phone: yup.number().min(10, "to short").required(),
   email: yup.string().email().required(),
@@ -59,121 +60,128 @@ export default function AddProductForm() {
           handleChange,
           handleBlur,
           handleSubmit,
-          setFieldValue,
           values,
           touched,
           errors,
           isValid,
         }) => (
-          <View style={styles.root}>
-            <View style={styles.formContainer}>
-              <TextInput
-                style={styles.formInput}
-                placeholder="title ?"
-                onChangeText={handleChange("name")}
-                onBlur={handleBlur("name")}
-                value={values.name}
-              ></TextInput>
+          <KeyboardAvoidingView
+            enabled={true}
+            behavior={Platform.OS === "ios" ? "padding" : "height"}
+            style={styles.root}
+          >
+            <ScrollView bounces={false} style={styles.flexOne}>
+              <View style={styles.formInputContainer}>
+                <TextInput
+                  placeholder="title ?"
+                  onChangeText={handleChange("name")}
+                  onBlur={handleBlur("name")}
+                  value={values.name}
+                  returnKeyType="next"
+                ></TextInput>
+              </View>
               {errors.name && touched.name && (
                 <Text style={styles.errors}>{errors.name}</Text>
               )}
 
-              <TextInput
-                style={styles.formInput}
-                placeholder="Price ?"
-                onChangeText={handleChange("price")}
-                onBlur={handleBlur("price")}
-                value={values.price.toString()}
-              ></TextInput>
+              <View style={styles.formInputContainer}>
+                <TextInput
+                  placeholder="Price ?"
+                  onChangeText={handleChange("price")}
+                  onBlur={handleBlur("price")}
+                  value={values.price.toString()}
+                  returnKeyType="next"
+                ></TextInput>
+              </View>
               {errors.price && touched.price && (
                 <Text style={styles.errors}>{errors.price}</Text>
               )}
 
-              <TextInput
-                style={styles.formInput}
-                placeholder="description ?"
-                onChangeText={handleChange("description")}
-                onBlur={handleBlur("description")}
-                value={values.description}
-              ></TextInput>
+              <View style={styles.formInputContainer}>
+                <TextInput
+                  placeholder="description ?"
+                  onChangeText={handleChange("description")}
+                  onBlur={handleBlur("description")}
+                  value={values.description}
+                  returnKeyType="next"
+                ></TextInput>
+              </View>
               {errors.description && touched.description && (
                 <Text style={styles.errors}>{errors.description}</Text>
               )}
 
-              <TextInput
-                style={styles.formInput}
-                placeholder="category ?"
-                onChangeText={handleChange("category")}
-                onBlur={handleBlur("category")}
-                value={values.category}
-              ></TextInput>
-
-              <Picker
-                enabled={true}
-                style={styles.formInput}
-                mode="dropdown"
-                onValueChange={handleChange("category")}
-                selectedValue={values.category}
-              >
-                {categories.map((item) => {
-                  return (
-                    <Picker.Item
-                      label={item.name.toString()}
-                      value={item.name.toString()}
-                      key={item.id.toString()}
-                    />
-                  );
-                })}
-              </Picker>
-
+              <View>
+                <Picker
+                  enabled={true}
+                  mode="dialog"
+                  dropdownIconColor={Theme.colors.buttonText}
+                  onValueChange={handleChange("category")}
+                  selectedValue={values.category}
+                >
+                  {categories.map((item) => {
+                    return (
+                      <Picker.Item
+                        label={item.name.toString()}
+                        value={item.name.toString()}
+                        key={item.id.toString()}
+                      />
+                    );
+                  })}
+                </Picker>
+              </View>
               {errors.category && touched.category && (
                 <Text style={styles.errors}>{errors.category}</Text>
               )}
-
-              <TextInput
-                style={styles.formInput}
-                placeholder="city ?"
-                onChangeText={handleChange("city")}
-                onBlur={handleBlur("city")}
-                value={values.city}
-              ></TextInput>
+              <View style={styles.formInputContainer}>
+                <TextInput
+                  placeholder="city ?"
+                  onChangeText={handleChange("city")}
+                  onBlur={handleBlur("city")}
+                  value={values.city}
+                  returnKeyType="next"
+                ></TextInput>
+              </View>
               {errors.city && touched.city && (
                 <Text style={styles.errors}>{errors.city}</Text>
               )}
 
-              <TextInput
-                style={styles.formInput}
-                placeholder="phone ?"
-                onChangeText={handleChange("phone")}
-                onBlur={handleBlur("phone")}
-                value={values.phone}
-              ></TextInput>
+              <View style={styles.formInputContainer}>
+                <TextInput
+                  placeholder="phone ?"
+                  onChangeText={handleChange("phone")}
+                  onBlur={handleBlur("phone")}
+                  value={values.phone}
+                  returnKeyType="next"
+                ></TextInput>
+              </View>
               {errors.phone && touched.phone && (
                 <Text style={styles.errors}>{errors.phone}</Text>
               )}
-
-              <TextInput
-                style={styles.formInput}
-                placeholder="email ?"
-                onChangeText={handleChange("email")}
-                onBlur={handleBlur("email")}
-                value={values.email}
-              ></TextInput>
+              <View style={styles.formInputContainer}>
+                <TextInput
+                  placeholder="email ?"
+                  onChangeText={handleChange("email")}
+                  onBlur={handleBlur("email")}
+                  value={values.email}
+                  returnKeyType="next"
+                ></TextInput>
+              </View>
               {errors.email && touched.email && (
                 <Text style={styles.errors}>{errors.email}</Text>
               )}
-
-              <TextInput
-                style={styles.formInput}
-                placeholder="Image ?"
-                onChangeText={handleChange("imageUri")}
-                onBlur={handleBlur("imageUri")}
-                value={values.imageUri}
-              ></TextInput>
+              <View style={styles.formInputContainer}>
+                <TextInput
+                  placeholder="Image ?"
+                  onChangeText={handleChange("imageUri")}
+                  onBlur={handleBlur("imageUri")}
+                  value={values.imageUri}
+                  returnKeyType="next"
+                ></TextInput>
+              </View>
               {errors.imageUri && touched.imageUri && (
                 <Text style={styles.errors}>{errors.imageUri}</Text>
               )}
-            </View>
+            </ScrollView>
 
             {/* ButtonSection for Android or IOS */}
             <View style={styles.center}>
@@ -200,7 +208,7 @@ export default function AddProductForm() {
                 />
               )}
             </View>
-          </View>
+          </KeyboardAvoidingView>
         )}
       </Formik>
     </>
@@ -209,27 +217,27 @@ export default function AddProductForm() {
 
 const styles = StyleSheet.create({
   root: {
-    flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
+    flexGrow: 1,
+    // height: "100%",
+    // width: "80%",
   },
-  formContainer: {
+  flexOne: {
+    flex: 1,
+    backgroundColor: Theme.colors.primary95,
+    borderRadius: 20,
+  },
+  formInputContainer: {
+    flex: 1,
+    borderRadius: 10,
+    height: 30,
+    margin: 20,
     justifyContent: "center",
-    alignItems: "center",
-    backgroundColor: "blue",
+    alignItems: "flex-start",
   },
   center: {
     justifyContent: "center",
     alignItems: "center",
     marginVertical: 15,
-  },
-  formInput: {
-    marginVertical: 8,
-    borderRadius: 8,
-    height: 38,
-    width: 200,
-    paddingHorizontal: 16,
-    backgroundColor: "#fff",
   },
   button: {
     width: 120,
@@ -241,10 +249,12 @@ const styles = StyleSheet.create({
   },
   buttonText: {
     fontSize: 20,
-    color: "#fff",
+    color: Theme.colors.buttonText,
     fontWeight: "bold",
   },
   errors: {
+    justifyContent: "center",
+    alignItems: "center",
     fontSize: 14,
     color: Theme.colors.bazaarRed,
     fontWeight: "500",
