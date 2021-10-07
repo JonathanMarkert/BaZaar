e6d80b3e22f2f49ad1461fb054bfc7f2d433cf1c
Alpha-85 <ljungdahlt@gmail.com>
2021-10-07 13:33:46 +0200

    Render screen depending on productSuccess is true/false

--- screens/AddProductSreen.tsx
+++ screens/AddProductSreen.tsx
@@ -3,20 +3,33 @@ import React from "react";
 import { ImageBackground, StyleSheet, View } from "react-native";
 import AddProductForm from "../components/AddProductForm";
 import Theme from "../components/Theme";
+import { useProductContext } from "../contexts/ProductContext";
+import SuccessScreen from "./SuccessScreen";
 
 export default function AddProductScreen() {
+  const { productSuccess, setProductSuccess } = useProductContext();
+
+  const toggleSuccess = () => {
+    setProductSuccess(!productSuccess);
+  };
   return (
-    <View style={styles.container}>
-      <StatusBar style="dark" />
-      <ImageBackground
-        source={require("../assets/bkg1.png")}
-        style={styles.backgroundImg}
-      >
-        <View style={styles.containerContent}>
-          <AddProductForm />
+    <>
+      {productSuccess == false ? (
+        <View style={styles.container}>
+          <StatusBar style="dark" />
+          <ImageBackground
+            source={require("../assets/bkg1.png")}
+            style={styles.backgroundImg}
+          >
+            <View style={styles.containerContent}>
+              <AddProductForm onSubmitSuccess={toggleSuccess} />
+            </View>
+          </ImageBackground>
         </View>
-      </ImageBackground>
-    </View>
+      ) : (
+        <SuccessScreen confirmSuccess={toggleSuccess} />
+      )}
+    </>
   );
 }
 
