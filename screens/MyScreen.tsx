f27a92d9ff99523f7dca17dfb3c07e9bb81f8390
Johanna Skinnari <jskinnari@gmail.com>
2021-10-09 08:55:27 +0200

    #136 remove outcommented code. And remove unused imports

--- screens/MyScreen.tsx
+++ screens/MyScreen.tsx
@@ -1,4 +1,3 @@
-import { CompositeScreenProps } from "@react-navigation/native";
 import { StatusBar } from "expo-status-bar";
 import React, { useContext } from "react";
 import {
@@ -6,20 +5,13 @@ import {
   ImageBackground,
   StyleSheet,
   Text,
-  View,
+  View
 } from "react-native";
 import ProductCard from "../components/ProductCard";
 import Theme from "../components/Theme";
 import { AuthContext } from "../contexts/AuthContext";
 import { IProduct, useProductContext } from "../contexts/ProductContext";
 import { MyScreenStackScreenProps } from "../navigation/MyScreenNavigatior";
-import { ProductsStackScreenProps } from "../navigation/ProductsNavigator";
-import { ScreenTabNavigationProp } from "../navigation/TabNavigator";
-
-// type Props = CompositeScreenProps<
-//   ScreenTabNavigationProp<"ProfileTab">,
-//   ProductsStackScreenProps<"Details">
-// >;
 
 export default function MyScreen({ navigation }: MyScreenStackScreenProps<"MyScreen">) {
   const { userToken } = useContext(AuthContext);
